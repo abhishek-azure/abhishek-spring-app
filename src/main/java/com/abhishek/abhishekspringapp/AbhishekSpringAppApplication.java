0679c3237e4ff3fab44efd51da54d5ec2d6541e5
= <=>
2022-03-09 13:23:23 +0530

    First commit

---
+++ src/main/java/com/abhishek/abhishekspringapp/AbhishekSpringAppApplication.java
@@ -0,0 +1,21 @@
+package com.abhishek.abhishekspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AbhishekSpringAppApplication {
+
+    @GetMapping("/message")
+    public String message() {
+        return "Congrats !! your app deployed successfully in Azure. ";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbhishekSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abhishek/abhishekspringapp/AbhishekSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.abhishekspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbhishekSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
