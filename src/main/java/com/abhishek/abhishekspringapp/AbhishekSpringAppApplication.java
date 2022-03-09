6fee2b04e1b860e3dca3d87477290f7194a999b6
= <=>
2022-03-09 14:31:04 +0530

    updated the message end point

--- src/main/java/com/abhishek/abhishekspringapp/AbhishekSpringAppApplication.java
+++ src/main/java/com/abhishek/abhishekspringapp/AbhishekSpringAppApplication.java
@@ -11,7 +11,7 @@ public class AbhishekSpringAppApplication {
 
     @GetMapping("/message")
     public String message() {
-        return "Congrats !! your app deployed successfully in Azure. ";
+        return "Congrats !! your application deployed successfully in Azure. ";
     }
 
     public static void main(String[] args) {
